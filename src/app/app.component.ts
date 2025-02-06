658f47771a81d272f24f0f5acd3a048f2d2aff68
Leonardo <leonardorh.ti22@utsjr.edu.mx>
2025-02-05 21:32:06 -0600

    Second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ejemplo4';
+
+  //Propiedades
+  listaTareas: string[];
+  tarea = new FormControl();
+
+  constructor(){
+    this.listaTareas = [];
+    let datos = localStorage.getItem('tareas');
+    if(datos != null){
+      let arregloTareas = JSON.parse(datos);
+      if(arregloTareas != null){
+        for(let tarea of arregloTareas){
+          this.listaTareas.push(tarea);
+        }
+      }
+    }
+  }
+
+  //Métodos
+  agregarTarea(){
+    this.listaTareas.push(this.tarea.value);
+    localStorage.setItem('tareas', JSON.stringify(this.listaTareas));
+    this.tarea.setValue('');
+  }
+
+  borrarTarea(posicion:number){
+    this.listaTareas.splice(posicion, 1);
+    localStorage.clear();
+    localStorage.setItem('tareas', JSON.stringify(this.listaTareas));
+  }
+
+  borrarTareas(){
+    localStorage.clear();
+    this.listaTareas = [];
+  }
 }
